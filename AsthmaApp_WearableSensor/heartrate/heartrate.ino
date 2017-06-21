5acf33938eba112dc11a1a2a22b23fb6fec3891b
Anjali Malik <anjali30malik@gmail.com>
2017-06-21 04:23:38 -0400

    Update heartrate.ino

--- AsthmaApp_WearableSensor/heartrate/heartrate.ino
+++ AsthmaApp_WearableSensor/heartrate/heartrate.ino
@@ -1,2 +1,66 @@
+//HEARTRATE
+#include <Wire.h>
+#include "MAX30105.h"
+#include "heartRate.h"
+MAX30105 particleSensor;
+
+const byte RATE_SIZE = 4; 
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time when last beat occurred
+float beatsPerMinute;
+int beatAvg;
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Initializing");
+
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST))
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
+  Serial.println("Place index finger on the sensor with steady pressure.");
+
+  particleSensor.setup();
+}
+
+void loop()
+{
+  long irValue = particleSensor.getIR();
+
+  if (checkForBeat(irValue) == true)
+  {
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    {
+      rates[rateSpot++] = (byte)beatsPerMinute;
+      rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
+  }
+
+  Serial.print("IR=");
+  Serial.print(irValue);
+  Serial.print(", BPM=");
+  Serial.print(beatsPerMinute);
+  Serial.print(", Avg BPM=");
+  Serial.print(beatAvg);
+
+  if (irValue < 50000)
+    Serial.print(" No finger detected");
+
+  Serial.println();
+}
 
 
